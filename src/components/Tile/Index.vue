691817ca2ebc7a97046db1330bda16c991267ab7
Sang Nguyen <sangnn2012@gmail.com>
2021-02-07 13:36:11 +0700

    basic UI

---
+++ src/components/Tile/Index.vue
@@ -0,0 +1,75 @@
+<template>
+  <div
+    class="tile flex flex-center"
+    :class="num !== -1 ? 'hasNum' : ''"
+    @click="$emit('onTileClicked')"
+  >
+    <div class="num" :class="num !== -1 ? 'hasNum' : ''">
+      {{ num }}
+    </div>
+    <q-icon v-show="tick && num !== -1" class="tick" name="done"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Tile",
+  props: {
+    num: {
+      type: Number,
+      default: -1
+    },
+    tick: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss">
+.tile {
+  -webkit-touch-callout: none; /* iOS Safari */
+  -webkit-user-select: none; /* Safari */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Old versions of Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+
+  user-select: none;
+  text-align: center;
+  position: relative;
+  border: 2px solid gray;
+  width: 8vw;
+  height: 10vw;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  padding: 5px;
+  background-color: #fa86c4;
+  pointer-events: none;
+  &.hasNum {
+    background-color: white;
+    cursor: pointer;
+    pointer-events: all;
+  }
+  .num {
+    font-size: 5vw;
+    color: transparent;
+
+    &.hasNum {
+      color: black;
+    }
+  }
+  .tick {
+    position: absolute;
+    color: green;
+    font-size: 8vw;
+    width: 100%;
+    top: 10%;
+    left: 0;
+  }
+}
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,9 +1,177 @@
 <template>
-  <q-page class="flex flex-center"> </q-page>
+  <q-page class="game-wrapper">
+    <div class="game-wrapper__title">
+      Chào mừng mấy con giời đến với Lô tô 2021
+    </div>
+    <div class="game-wrapper__main">
+      <div
+        class="row"
+        :class="(rowIndex + 1) % 3 === 0 ? 'endOfSection' : ''"
+        v-for="(row, rowIndex) of board"
+        :key="'row' + rowIndex"
+      >
+        <tile
+          v-for="(cell, cellIndex) of row"
+          :key="'cell' + rowIndex + cellIndex"
+          :num="cell.num"
+          :tick="cell.tick"
+          @onTileClicked="onTileClicked(rowIndex, cellIndex)"
+        ></tile>
+      </div>
+      <!-- <tile v-for="cell of row" :num="cell.num" :tick="cell.tick"></tile> -->
+    </div>
+  </q-page>
 </template>
 
 <script>
+import Tile from "src/components/Tile/Index.vue";
 export default {
-  name: "PageIndex"
+  name: "PageIndex",
+  components: {
+    Tile
+  },
+  mounted() {
+    this.generateBoard();
+  },
+  data() {
+    return {
+      board: [
+        [
+          { num: -1 },
+          { num: 18, tick: false },
+          { num: 22, tick: false },
+          { num: -1 },
+          { num: -1 },
+          { num: 55, tick: false },
+          { num: -1 },
+          { num: 76, tick: true },
+          { num: 83, tick: true }
+        ],
+        [
+          { num: -1 },
+          { num: 12, tick: true },
+          { num: -1 },
+          { num: 38, tick: false },
+          { num: 40, tick: false },
+          { num: -1 },
+          { num: 66, tick: true },
+          { num: -1 },
+          { num: 82, tick: false }
+        ],
+        [
+          { num: 1, tick: false },
+          { num: -1 },
+          { num: 27, tick: true },
+          { num: -1 },
+          { num: 42, tick: false },
+          { num: -1 },
+          { num: -1 },
+          { num: 73, tick: false },
+          { num: 85, tick: false }
+        ],
+        [
+          { num: -1 },
+          { num: 18, tick: false },
+          { num: 22, tick: false },
+          { num: -1 },
+          { num: -1 },
+          { num: 55, tick: false },
+          { num: -1 },
+          { num: 76, tick: true },
+          { num: 83, tick: true }
+        ],
+        [
+          { num: -1 },
+          { num: 12, tick: true },
+          { num: -1 },
+          { num: 38, tick: false },
+          { num: 40, tick: false },
+          { num: -1 },
+          { num: 66, tick: true },
+          { num: -1 },
+          { num: 82, tick: false }
+        ],
+        [
+          { num: 1, tick: false },
+          { num: -1 },
+          { num: 27, tick: true },
+          { num: -1 },
+          { num: 42, tick: false },
+          { num: -1 },
+          { num: -1 },
+          { num: 73, tick: false },
+          { num: 85, tick: false }
+        ],
+        [
+          { num: -1 },
+          { num: 18, tick: false },
+          { num: 22, tick: false },
+          { num: -1 },
+          { num: -1 },
+          { num: 55, tick: false },
+          { num: -1 },
+          { num: 76, tick: true },
+          { num: 83, tick: true }
+        ],
+        [
+          { num: -1 },
+          { num: 12, tick: true },
+          { num: -1 },
+          { num: 38, tick: false },
+          { num: 40, tick: false },
+          { num: -1 },
+          { num: 66, tick: true },
+          { num: -1 },
+          { num: 82, tick: false }
+        ],
+        [
+          { num: 1, tick: false },
+          { num: -1 },
+          { num: 27, tick: true },
+          { num: -1 },
+          { num: 42, tick: false },
+          { num: -1 },
+          { num: -1 },
+          { num: 73, tick: false },
+          { num: 85, tick: false }
+        ]
+      ]
+    };
+  },
+  methods: {
+    onTileClicked(rowIndex, cellIndex) {
+      this.board[rowIndex][cellIndex].tick = !this.board[rowIndex][cellIndex]
+        .tick;
+    },
+    generateBoard() {
+
+    }
+  }
 };
 </script>
+
+<style lang="scss" scoped>
+.game-wrapper {
+  &__title {
+    text-align: center;
+    font-size: 24px;
+  }
+  &__main {
+    width: 90%;
+    margin: auto;
+    margin-top: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    .row {
+      width: 100%;
+      margin: auto;
+      justify-content: center;
+      &.endOfSection {
+        margin-bottom: 20px;
+      }
+    }
+  }
+}
+</style>
